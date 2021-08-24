96c1c2e2a71a3239cdd999d541645547a9ce9b59
Sanskar Mohapatra <sanskarmohapatra@gmail.com>
2021-08-25 01:03:25 +0530

    frontend final

--- src/variables.js
+++ src/variables.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://foodibackend.herokuapp.com";
 export const CLIENT_ID =
   "356314602000-2fprv1e8qel1plrcrubmbpkggm328nn3.apps.googleusercontent.com";
